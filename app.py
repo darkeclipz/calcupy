db7dc8b260c6cf6452fb57a91eb978d0d600b463
darkeclipz <larsrotgers@gmail.com>
2018-12-18 22:41:18 +0100

    pplot speed

--- app.py
+++ app.py
@@ -340,7 +340,7 @@ def pplot():
         if not 'Matrix' in str(type(ps)):
             raise ValueError('Parametric plot requires a matrix.')
         
-        a = 32
+        a = 24
         fig = plt.figure(figsize=(5,5))
         fig.clf()
 
